36914f1d3f3259015b320ad3de7ac9c8550042c5
dhiraj424 <kumardhiraj424@gmail.com>
2026-03-25 16:37:06 +0530

    Delete prediction_result.sql

--- prediction_result.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-  fullVisitorId,
-  predicted_label,
-  ROUND(p.predicted_label_probs[OFFSET(0)].prob, 4) AS purchase_probability
-FROM
-  ML.PREDICT(MODEL `ecommerce_data.conversion_model`, (
-    SELECT * FROM `ecommerce_data.training_view`
-  )) AS p
-LIMIT 10;
